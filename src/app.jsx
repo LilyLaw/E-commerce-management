9801bdb2d2594891a30c795462bf22a07bc6fd22
LilyLaw <1444987087@qq.com>
2019-03-20 17:29:24 +0800

    webpack配置完成且初始化

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Icon } from 'antd';
+
 import './index.css'
 import './index.scss'
 
 ReactDOM.render(
-		<h1>Hello React!</h1>,
+		<h1>			Hello React!
+		</h1>,
 		document.getElementById("app")
 	)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path 				= require('path');
+const webpack			= require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
@@ -6,15 +7,9 @@ module.exports = {
 	entry: './src/app.jsx',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'app.js'
+		publicPath:'/dist/',
+		filename: 'js/app.js'
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title:"电商管理平台",
-			template:"./src/index.html"
-		}),
-		new ExtractTextPlugin("index.css"),
-	],
 	module: {
 		rules: [
 			// react 处理
@@ -43,7 +38,49 @@ module.exports = {
 		    	    fallback: 'style-loader',
 		        	use: ['css-loader', 'sass-loader']
 		        })
-		    }
+		    },
+		    // 图片
+		    {
+		        test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+	      	},
+	      	{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					{
+						loader:'file-loader',
+						options: {
+						  	limit: 8192,
+						  	name:'resource/[name].[ext]'
+						}
+					}
+				]
+			}
 		]
+	},
+	plugins: [
+		// 处理html文件
+		new HtmlWebpackPlugin({
+			title:"电商管理平台",
+			template:"./src/index.html"
+		}),
+		// 独立css文件
+		new ExtractTextPlugin("css/[name].css"),
+		// 提出公共模块
+		new webpack.optimize.CommonsChunkPlugin({
+			name:'common',
+			filename:'js/base.js'
+		})
+	],
+	devServer: {
+    	port:3010
 	}
 };
