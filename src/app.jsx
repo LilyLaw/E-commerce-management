6eef415030a824ea63dbd2016832cbd90c53bb2a
LilyLaw <1444987087@qq.com>
2019-03-30 17:08:17 +0800

    '改下目录结构'

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import IndexMenu from 'component/layout/indexMenu.jsx';
 import HeaderMenu from 'component/layout/headerMenu.jsx';
-import LllRoute from 'page/lllroute.jsx';
+import LllRoute from './lllroute.jsx';
 import NormalLoginForm from 'page/login/index.jsx';
 import { Layout } from 'antd';
 import 'antd/dist/antd.css'
-import 'component/style.scss';
+import './style.scss';
 
 const {
   Header, Footer, Sider, Content,
