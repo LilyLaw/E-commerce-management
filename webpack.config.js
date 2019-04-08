1e7433a28ebe1f66c95dbdf4f6b09fb63a3329a9
LilyLaw <1444987087@qq.com>
2019-04-08 17:17:22 +0800

    nginx配置有问题，暂时不为静态资源分配域名

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
 	entry: './src/app.jsx',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		publicPath: WEBPACK_ENV ==='dev' ? '/dist/' : '//static.lulilin.top/lllmanage/dist',
+		publicPath: WEBPACK_ENV ==='dev' ? '/dist/' : '/dist/',
+		// publicPath: WEBPACK_ENV ==='dev' ? '/dist/' : '//static.lulilin.top/E-commerce-management/dist',
 		filename: 'js/app.js'
 	},
 	resolve:{
