75dfa4bc9e75683b0ac905ba0af58947bbd37385
LilyLaw <1444987087@qq.com>
2019-04-08 17:55:19 +0800

    ng

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	entry: './src/app.jsx',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		publicPath: WEBPACK_ENV ==='dev' ? '/dist/' : '/dist/',
+		publicPath: WEBPACK_ENV ==='dev' ? '/dist/' : '',
 		// publicPath: WEBPACK_ENV ==='dev' ? '/dist/' : '//static.lulilin.top/E-commerce-management/dist',
 		filename: 'js/app.js'
 	},
